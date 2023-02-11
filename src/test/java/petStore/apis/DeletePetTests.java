73b37dfeaa9800d53b8b4a0a982ea8de6b92ef43
Laser <laser09@gmail.com>
2023-02-11 20:49:35 +0100

    Modifying code description in DeletePetTests class

--- src/test/java/petStore/apis/DeletePetTests.java
+++ src/test/java/petStore/apis/DeletePetTests.java
@@ -19,33 +19,33 @@ public class DeletePetTests extends TestBaseClassAPI {
     public void deletingExistingRecord (){
 
         /* Tworzę nowy obiekt klasy "PostPet", dzięki temu uzyskam dostęp do metod zawartych w tej klasie.
-         Obiekt użyję jako jeden z argumentów metody "getResponsePostSuccessTest". */
+         Obiektu użyję jako jeden z argumentów metody "getResponsePostSuccess". */
 
         PostPet postPet = new PostPet();
 
         /* Tworzę nowy obiekt klasy "DeletePet", którego użyję jako jeden z argumentów w metodzie
-         "getResponseDeleteTest" do wysłania zapytania do API. */
+         "getResponseDelete" do wysłania żądania do API. */
 
         DeletePet deletePet = new DeletePet();
 
         /* Tworzę nowy obiekt klasy "SoftAssertions", którego metod użyję do potwierdzenia zgodności otrzymanych
-          danych w stosunku do oczekiwanych. */
+          danych z oczekiwanymi. */
 
         SoftAssertions softly = new SoftAssertions();
 
-        /* Tworzę nowy obiekt klasy "Map", który jest interfejsem służącym do przechowywania danych w
-          postaci klucz- wartość. Obiekt klasy "String" do którego mogę przypisać dane w postaci
-          ciągu znaków posłuży mi jako parametr. Przekazuję go jako drugi argument (wartość) metody "put"
-          wywołanej na obiekcie paramsMap. Pierwszym argumentem (klucz) jest wartość "id". */
+        /* Tworzę nowy obiekt klasy "Map", który jest interfejsem służącym do przechowywania danych w postaci
+          klucz- wartość. Obiekt klasy "String" do którego mogę przypisać dane w postaci ciągu znaków przekazuję
+          jako drugi argument (wartość) metody "put" wywołanej na obiekcie paramsMap. Pierwszym argumentem (klucz)
+          jest "id". */
 
         Map<String,String> paramsMap = new HashMap<>();
         String id = "1234";
         paramsMap.put("id",id);
 
         /* Tworzę nowy obiekt klasy "ArrayList", który jest implementacją interfejsu "List". Klasa ta umożliwia
-          przechowywanie danych w tablicy. Obiekt tagList przechowuje obiekty typu "Tag". Tworząc nowy obiekt klasy
+          przechowywanie danych w tablicy. Obiekt "tagList" przechowuje obiekty typu "Tag". Tworząc nowy obiekt klasy
           "Tag" uzyskuję dostęp do jego metod. Wykorzystuję metody "setId" oraz "setName" do zdefiniowania właściwości
-          obiektu firstTag. Następnie wywołuję metodę "add" na obiekcie tagList i przekazuję do niego obiekt firstTag.    */
+          obiektu firstTag. Następnie wywołuję metodę "add" na obiekcie tagList i przekazuję do niego obiekt firstTag. */
 
         List<Tag> tagList = new ArrayList<>();
         Tag firstTag = new Tag();
@@ -53,34 +53,37 @@ public class DeletePetTests extends TestBaseClassAPI {
         firstTag.setName("First Pet");
         tagList.add(firstTag);
 
-        /* Tworzę nowy obiekt klasy "Category" i dzięki dostępowi do jego metod używam "setId" i "setName" ustawiam
+        /* Tworzę nowy obiekt klasy "Category" i dzięki dostępowi do jego metod używam "setId", "setName" do ustawienia
           jego właściwości.  */
 
         Category category = new Category();
         category.setId(1231321L);
         category.setName("Nazwa");
 
-        /* Do wcześniej utworzonego obiektu typu "postPet". Do ustawienia jego właściwości używam metod zawartych
-          w klasie "postPet". Do testu wykorzystuję wybranych metod, tj. "setId","setName", "setStatus". Dodatkowo
-          przy użyciu metod "setCategory" i "setTags" przekazuję wcześniej utworzone i zdefiniowane obiekty "tagList"
-          oraz "category". Tak zdefiniowany obiekt "postPet" przekazuję jako argument w metodzie "getResponsePostSuccessTest",
-          której zadaniem jest wysłanie wniosku do API o utworzenie pozycji o wcześniej zdefiniowanych informacjach. */
+        /* Ustawiam właściwości wcześniej utworzonego obiektu typu "postPet" przy pomocy metod zawartych w klasie "PostPet".
+          Do tego testu wykorzystuję wybranych metod, tj. "setId","setName", "setStatus". Dodatkowo przy użyciu metod
+          "setCategory" i "setTags" przekazuję wcześniej utworzone i zdefiniowane obiekty "tagList" oraz "category". Tak
+          zdefiniowany obiekt "postPet" przekazuję jako argument w metodzie "getResponsePostSuccess", której zadaniem
+          jest wysłanie żądania do API o utworzenie pozycji o właściowściach zdefiniowanych w obiekcie "postPet". */
 
         postPet.getRequestBody().setId(Long.parseLong(id));
         postPet.getRequestBody().setName("Łajka");
         postPet.getRequestBody().setTags(tagList);
         postPet.getRequestBody().setCategory(category);
         postPet.getRequestBody().setStatus(Pet.StatusEnum.AVAILABLE);
-        getResponsePostSuccessTest(postPet);
+        getResponsePostSuccess(postPet);
 
-        /* Metody "getResponseDeleteTest" używam do wysłania wniosku do API o usunięcie konkretnej pozycji. W argumentach
-          przekazuję obiekty "deletePet", "paramsMap" a także kod oczekiwanej odpowiedzi z API. */
+        /* Metody "getResponseDelete" używam do wysłania żądania do API o usunięcie konkretnej pozycji. Obiekt "deletePet",
+          który służy do opakowania informacji potrzebnych do wykonania żądania "DELETE" jest pierwszym argumentem tej metody.
+          Obiekt "paramsMap" zawiera parametry, które zostaną dodane do ścieżki URL przekazuję jako drugi argument. Trzecim
+          argumentem jest "statusCode",czyli kod oczekiwanej przeze mnie odpowiedzi HTTP, tj. 200. */
 
-        getResponseDeleteTest(deletePet,paramsMap,200);
+        getResponseDelete(deletePet,paramsMap,200);
 
         /* Używam metod zawartych w klasie "DeletePet" na obiekcie deletePet do uzyskania dostępu do danych zawartych w
-          ciele odpowiedzi z API. Przy pomocy metod klasy "SoftAssertions" weryfikuję czy dane, które otrzymałem są zgodne
-          z tymi, których oczekiwałem. */
+          "body" odpowiedzi z API. Dzięki metodom "getCode", "getType" i "getMessage" pobieram dane z "body" odpowiedzi.
+          Nastepnie przy pomocy metody "isEqual" klasy "SoftAssertions" weryfikuję czy otrzymane dane są zgodne z tymi,
+          których oczekiwałem. */
 
         softly.assertThat(deletePet.getResponseBody().getCode()).isEqualTo(200);
         softly.assertThat(deletePet.getResponseBody().getType()).isEqualTo("unknown");
@@ -88,4 +91,5 @@ public class DeletePetTests extends TestBaseClassAPI {
         softly.assertAll();
 
     }
+
 }
