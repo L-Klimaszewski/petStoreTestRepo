04d75c3c3758b81cde34d90d20cb8a7cc6c9b218
Laser <laser09@gmail.com>
2023-02-15 11:55:29 +0100

    Adding methods to open website and accept cookies

--- src/test/java/otoMoto/pl/frontEnd/MainPageTests.java
+++ src/test/java/otoMoto/pl/frontEnd/MainPageTests.java
@@ -1,4 +1,24 @@
 package otoMoto.pl.frontEnd;
 
-public class MainPageTests {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import otoMoto.pl.TestBaseClassFrontEnd;
+
+public class MainPageTests extends TestBaseClassFrontEnd {
+
+
+
+    @Test
+    public void testingOtoMotoMainPage(){
+
+        // Wysyłam żądanie do przeglądarki, aby otworzyła podaną stronę.
+        driver.get("https://otomoto.pl");
+
+        // Wyszukuję element na stronie o konkretnym ID, a następnie klikam w niego.
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+
+
+
+    }
+
 }
