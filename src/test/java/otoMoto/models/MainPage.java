ccfbc5c7ec407e8b87d0fb684844722415d0ab8a
Laser <laser09@gmail.com>
2023-02-18 18:50:23 +0100

    Adding code description.

--- src/test/java/otoMoto/models/MainPage.java
+++ src/test/java/otoMoto/models/MainPage.java
@@ -6,12 +6,23 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import otoMoto.TestBaseClassFrontEnd;
 
+    /* Klasa "MainPage" jest reprezentacją głównej strony, którą testuję. Jest klasą pochodną klasy
+     "TestBaseClassFrontEnd" co pozwala mi na korzystanie z metod w niej zawartych. Klasa "MainPage"
+     zawiera elementy strony  */
+
 public class MainPage extends TestBaseClassFrontEnd {
 
+    /* Konstruktor klasy "MainPage" zostanie wywoływany podczas tworzenia nowego obiektu tej klasy.
+     Przyjmuje za argument obiekt klasy "WebDriver" o nazwie 'driver'. Obiekt 'driver' przypisany
+     jest do pola 'driver' wewnątrz obiektu klasy "MainPage". */
     public MainPage (WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
+
+    /* Poszczególne elementy strony identyfikowane po ich unikalnych atrybutach takich jak id, xpath itp.
+     Są to elementy, których używam w testach. Adnotacje @FindBy pozwalają mi na łatwe zlokalizowanie
+     elementów strony i użycie ich w metodach klas. */
 
     @FindBy (id = "onetrust-accept-btn-handler")
     public WebElement cookies;
@@ -36,6 +47,8 @@ public class MainPage extends TestBaseClassFrontEnd {
     @FindBy (xpath = "//*[@id='__next']/div/div/div/div[2]/div[1]/div/form/section/div/div[5]/div/div[2]/div/div/input")
     public WebElement chosenMaxPrice;
 
+
+    // Metoda otwiera stronę główną otomoto.pl z użyciem obiektu WebDriver.
     public void getToMainPage(){
         driver.get("https://otomoto.pl");
     }
